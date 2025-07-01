6eabd0a537c9578a38e647574320f82652938906
tiger bima perkasa <tigerbimaperkasa@gmail.com>
2025-07-01 03:23:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/map-loader.tsx
+++ src/components/map-loader.tsx
@@ -1,13 +1,17 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import { useMemo } from 'react';
 import { Skeleton } from '@/components/ui/skeleton';
 
-const VendorMap = dynamic(() => import('@/components/vendor-map'), {
-  ssr: false,
-  loading: () => <Skeleton className="h-[500px] w-full rounded-lg" />,
-});
-
 export default function MapLoader() {
-  return <VendorMap />;
+  const Map = useMemo(() => dynamic(
+    () => import('@/components/vendor-map'),
+    {
+      ssr: false,
+      loading: () => <Skeleton className="h-[500px] w-full rounded-lg" />,
+    }
+  ), []);
+
+  return <Map />;
 }
